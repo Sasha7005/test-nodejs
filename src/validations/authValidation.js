b578b5543af6bc8ed074e82d58e76d7aa6f06f8a
Bas <basarab.o@icloud.com>
2026-03-10 14:17:18 +0200

    5.9

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -15,3 +15,18 @@ export const loginUserSchema = {
     password: Joi.string().required(),
   }),
 };
+// src/validations/authValidation.js
+
+// Решта коду файла
+
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).required(),
+    token: Joi.string().required(),
+  }),
+};
